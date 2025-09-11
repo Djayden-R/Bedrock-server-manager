be0567ded2fd2f3cf43ac511d6362440e91ffe41
Djayden-R <6953@rgomiddelharnis.nl>
2025-09-11 16:57:37 +0200

    Add config loader

---
+++ load_config.py
@@ -0,0 +1,29 @@
+import dotenv
+import yaml
+
+class Import_variables:
+    def __init__(self):
+        self.yaml_values = self.load_yaml()
+        self.env_values = self.load_env()
+
+    def load_yaml(yaml_path):
+        with open(yaml_path, "r", encoding="utf-8") as f:
+            return yaml.safe_load(f)
+
+    def load_env():
+        env_path = dotenv.find_dotenv(usecwd=True)
+        values = dict(dotenv.dotenv_values(env_path))
+        return values
+    
+    def secret(self, key):
+        try:
+            return self.env_values(key)
+        except ValueError:
+            print(f"Key is invalid: {key} does not exist in .env")
+    
+    def secret(self, key):
+        try:
+            return self.yaml_values(key)
+        except ValueError:
+            print(f"Key is invalid: {key} does not exist in .env")
+    
