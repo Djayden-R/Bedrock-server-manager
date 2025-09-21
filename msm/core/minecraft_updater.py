a4a11c0ae85db9dc24d7b96cc9479cf725194468
Djayden-R <6953@rgomiddelharnis.nl>
2025-09-21 13:47:53 +0200

    make minecraft_updater more versatile by lettting it downlaod multiple files

--- msm/core/minecraft_updater.py
+++ msm/core/minecraft_updater.py
@@ -2,22 +2,28 @@ from github import Github, Auth
 import requests
 import os
 import subprocess
+import sys
+
+# Add the project root to Python path
+project_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.insert(0, project_root)
+
 from msm.config.load_config import Config
 
-repo_name = "MCXboxBroadcast/Broadcaster"
+bedrockbot_repo = "MCXboxBroadcast/Broadcaster"
 
 cfg = Config()
-GITHUB_TOKEN = cfg.secret("github_token")
-BEDROCK_BOT_PATH = cfg.yaml("bedrock_bot_path")
+GITHUB_TOKEN = cfg.load("github_token")
+BEDROCK_BOT_PATH = cfg.load("bedrock_bot_path")
 
-def download(url, name):
+def download(url, file_path):
         r = requests.get(url, stream=True)
 
-        with open(name, "wb") as f:
+        with open(file_path, "wb") as f:
             for chunk in r.iter_content(chunk_size=8192):
                 f.write(chunk)
 
-def get_latest_release(repo_name, filename):
+def get_latest_release(repo_name, download_location, filename=None):
     # using an access token
     auth = Auth.Token(GITHUB_TOKEN)
 
@@ -28,28 +34,37 @@ def get_latest_release(repo_name, filename):
     latest_version = repo.get_latest_release()
 
     for asset in latest_version.get_assets():
-        if asset.name == filename:
-            download(asset.browser_download_url, filename)
-            if os.path.exists(f"{os.path.dirname(os.path.abspath(__file__))}/{filename}"):
+
+        if asset.name == filename or filename == None:
+
+            #location fo where file will be saved
+            file_path = f"{download_location}/{asset.name}"
+
+            #remove file if it already exists
+            if os.path.exists(file_path):
+                subprocess.run(["rm", file_path])
+            
+            #download file to it's location
+            download(asset.browser_download_url, file_path)
+
+            #check if file downloaded successfully
+            if os.path.exists(file_path):
                 print("File is successfully downloaded")
             else:
                 print("Problem during download")
-            break
 
     # To close connections after use
     g.close()
 
-def update_bedrock_bot():
-    get_latest_release(repo_name, "MCXboxBroadcastStandalone.jar")
-    subprocess.run(["rm", f"{BEDROCK_BOT_PATH}/MCXboxBroadcastStandalone.jar"])
-    subprocess.run(["mv", "MCXboxBroadcastStandalone.jar", BEDROCK_BOT_PATH])
+def get_bedrock_bot():
+    get_latest_release(bedrockbot_repo, BEDROCK_BOT_PATH , filename="MCXboxBroadcastStandalone.jar")
 
 def update_minecraft_server():
     minecraft_updater_path = os.path.expanduser("~/minecraft_server/updater/mcserver_autoupdater.py")
     subprocess.run(['python3', minecraft_updater_path])
 
 def main():
-    update_bedrock_bot()
+    get_bedrock_bot()
     update_minecraft_server()
 
 if __name__ == "__main__":
