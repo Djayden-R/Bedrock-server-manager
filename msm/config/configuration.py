9069b21d09148b34f5ffeda0c14709da47e9bf99
Djayden-R <6953@rgomiddelharnis.nl>
2025-09-25 16:57:21 +0200

    refactor configuration setup and add backup options

--- msm/config/configuration.py
+++ msm/config/configuration.py
@@ -11,16 +11,6 @@ sys.path.insert(0, project_root)
 import msm.core.minecraft_updater
 
 #setup file for new users
-
-"""
-Checklist:
-asking variables:
-  - minecraft server ip
-  - minecraft shutdown time
-  - github token
-  - dyna pass and ip
-  - HA server (ip and button id's)
-"""
 def linux_check():
     if sys.platform != "linux":
         print("You are not running Linux. This program will not work as expected.")
@@ -78,15 +68,63 @@ def shutdown_mode_setup():
     clear_console()
     print("This code is made to shutdown your server after a set amount of time where no one is online")
     if questionary.confirm("Would you like to enable that?").ask():
-        shutdown_mode = True
         shutdown_time = questionary.text(
             "After how many minutes of inactivity should the server shutdown?",
             validate=lambda val: val.isdigit() or "Enter a number"
             ).ask()
     else:
-        shutdown_mode, shutdown_time = False, None
+        shutdown_time = None
     
-    return shutdown_mode, shutdown_time
+    return shutdown_time
+
+def automatic_backups_setup(default_path):
+    clear_console()
+    backup_options = questionary.checkbox(
+        "There are different options for automatic backups, select the ones you want to use:",
+        choices=["Local backup", "Back up to external drive", "Drive backup"]
+    ).ask()
+    
+    local_backup = "Local backup" in backup_options
+    hdd_backup = "Back up to external drive" in backup_options
+    drive_backup = "Drive backup" in backup_options
+
+    if local_backup:
+        local_backup_path = questionary.path(
+            "Where do you want to save the local backups?",
+            default=os.path.join(default_path, "backups"),
+            only_directories=True
+        ).ask()
+    else:
+        local_backup_path = None
+    
+    if hdd_backup:
+        hdd_backup_path = questionary.path(
+            "Where do you want to save the external drive backups?",
+            default="/mnt",
+            only_directories=True
+        ).ask() 
+    else:
+        hdd_backup_path = None
+    if drive_backup:
+        print("You will need rclone for drive backups, so make sure you have it set up")
+        drive_backup_path = questionary.text(
+            "What is the name of your rclone remote? (something like 'drive:')",
+            validate=lambda val: val.endswith(":") or "Remote name must end with ':'"
+        ).ask()
+    else:
+        drive_backup_path = None
+
+    backup_directories = []
+    while True:
+        directory = questionary.path(
+            "Enter a directory to back up (or leave blank to finish):",
+            only_directories=True,
+            default=default_path
+        ).ask()
+        if not directory:
+            break
+        backup_directories.append(directory)
+    return local_backup_path, hdd_backup_path, drive_backup_path, backup_directories
 
 def main():
     print("Hi, there!")
@@ -97,7 +135,7 @@ def main():
     clear_console()
 
     program_location = os.path.realpath(__file__).removesuffix("\\msm\\config\\configuration.py")
-    if questionary.confirm(f"Are you sure you want to use this: {program_location} location for this program?").ask():
+    if questionary.confirm(f"Are you sure you want to use this location:\n {program_location} \nfor this program?").ask():
         print("Great, let's continue")
     else:
         print("Please move this program to the location you want to use and run it again.")
@@ -106,46 +144,58 @@ def main():
     
     questionary.press_any_key_to_continue("I am going to ask you a few questions to set everything up.").ask()
 
+    services = questionary.checkbox(
+    "What services do you want to set up?", choices=["Home Assistant", "Dynu DNS", "Automatic shutdown", "Automatic backups"]).ask()
 
-    home_assistant = questionary.confirm("Do you want to use Home Assistant?").ask()
 
-    dynu = questionary.confirm("Do you want to set-up dynu for dynamic dns?").ask()
+    home_assistant = "Home Assistant" in services
+    dynu = "Dynu DNS" in services
+    auto_shutdown = "Automatic shutdown" in services
+    auto_backup = "Automatic backups" in services
 
-    variables_to_save = {}
+
+    #gather all variables and save them to a dictionary
+    config_data = {}
 
     if home_assistant:
-        home_assistant_ip, home_assistant_token = home_assistant_setup()
-        active, ip, token =  True, home_assistant_ip, home_assistant_token
-    else:
-        active, ip, token = False, None, None
-    
-    variables_to_save["HOME_ASSISTANT"] = {"active": active, "ip": ip, "token": token}
+        ha_ip, ha_token = home_assistant_setup()
+        config_data["home_assistant"] = {"active": True, "ip": ha_ip, "token": ha_token}
 
     if dynu:
         dynu_password, dynu_domain = dynu_setup()
-        active, password, domain = True, dynu_password, dynu_domain
-    else:
-        active, password, domain = False, None, None
-    
-    variables_to_save["DYNU"] = {"active": active, "password": password, "domain": domain}
+        config_data["dynu_dns"] = {"active": True, "password": dynu_password, "domain": dynu_domain}
+
+    if auto_shutdown:
+        shutdown_time = shutdown_mode_setup()
+        config_data["shutdown"] = {"shutdown_time": shutdown_time}
+
+    if auto_backup:
+        local_backup_path, hdd_backup_path, drive_backup_path, backup_directories = automatic_backups_setup(program_location)
+        config_data["automatic_backups"] = {k: v for k, v in [("local_backup_path", local_backup_path), ("hdd_backup_path", hdd_backup_path), ("drive_backup_path", drive_backup_path), ("backup_directories", backup_directories)] if v is not None}
 
     clear_console()
 
-    print("Now that we have all of the services set-up, let's set up your minecraft server")
-
-    shutdown_mode, shutdown_time = shutdown_mode_setup()
-    variables_to_save["SHUTDOWN"] = {"auto_shutdown": shutdown_mode, "shutdown_time": shutdown_time}
-    #install the minecraft server, install the minecraft bot
-
-    print("Now that we have all those values let's save them to a .yaml file")
-    print("You should never share this file as it will give access to all of the services you added just now")
-
     with open('msm/config/config.yaml', 'w') as f:
-        yaml.dump(variables_to_save, f, default_flow_style=False, indent=2)
+        yaml.dump(config_data, f, default_flow_style=False, indent=2)
+        print("Config file saved to msm/config/config.yaml")
+
+    if dynu or home_assistant:
+        print("Never share this file with anyone as it will give access to all of the services you configured")
     
-    print("Now we just download the required repositories")
-    msm.core.minecraft_updater.get_bedrock_bot()
-    msm.core.minecraft_updater.get_minecraft_updater()
+    #we don't have a config yet, so we need to pass a fake config to the functions
+    class FakeConfig:
+        def __init__(self):
+            self.bedrock_bot_path = os.path.join(project_root, "bedrock_connector")
+            self.mc_updater_path = os.path.join(project_root, "minecraft_updater")
+    cfg = FakeConfig()
+
+    #ask permission to download the repositories
+    print("This program can uses MCXboxBroadcast/Broadcaster to make it possible for console players to join the server")
+    print("This is optional")
+    if questionary.confirm("Do you want to download this program?").ask():
+        print("Great, downloading now")
+        msm.core.minecraft_updater.get_bedrock_bot(cfg)
+    msm.core.minecraft_updater.get_minecraft_updater(cfg)
 
 if __name__ == "__main__":
     main()
