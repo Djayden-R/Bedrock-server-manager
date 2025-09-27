a56b7cd5628a6daad49f0ed3de4b0c8e2ace6491
Djayden-R <6953@rgomiddelharnis.nl>
2025-09-27 13:33:25 +0200

    Add check for new user and pass config to entity_status()

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ import msm.core.backup as backup
 import sys
 from datetime import datetime
 from enum import Enum
-
-cfg = Config.load()
+from pathlib import Path
 
 class Mode(Enum):
     NORMAL = "normal" #normal operating mode, shutdown after 3 minutes with local backup and hdd backup
     DRIVE_BACKUP = "drive backup" #just backup to google drive, then shutdown
     INVALID = "invalid time" #boot up at an invalid time, just shutdown
     UPDATE = "update" #update the server
+    CONFIGURATION = "config" #go through set-up process
 
 def shutdown():
 	with open("/tmp/mc_ready","w") as f:
@@ -23,6 +23,15 @@ def hour_valid(hour):
     return cfg.timing_begin_valid < hour < cfg.timing_end_valid
 
 def get_mode():
+    #check if new user
+    config_file = Path("msm/config/config.yaml").absolute()
+    if not config_file.exists():
+        print(f"[{datetime.now()}] no config found, running set-up process")
+        return Mode.CONFIGURATION
+    else:
+        global cfg 
+        cfg = Config.load()
+
     time = datetime.now()
     hour = time.hour
     print(f"[{datetime.now()}] current hour: {hour}")
@@ -31,7 +40,7 @@ def get_mode():
             return Mode.UPDATE
         else:
             return Mode.NORMAL
-    elif hour == cfg.backup_time:
+    elif hour == cfg.timing_drive_backup:
         return Mode.DRIVE_BACKUP
     else:
         return Mode.INVALID
@@ -40,21 +49,21 @@ def normal_shutdown():
     update_DNS(cfg)
     while True:
         if start_checking_playercount(cfg):
-            if entity_status():
+            if entity_status(cfg, cfg.ha_shutdown_entity):
                 print(f"[{datetime.now()}] starting backup script")
-                backup.main(type="quick")
+                backup.main(cfg, type="quick")
                 print(f"[{datetime.now()}] shutting down...")
                 shutdown()
                 sys.exit(42)
         else:
-            if entity_status():
+            if entity_status(cfg, cfg.ha_shutdown_entity):
                 print(f"[{datetime.now()}] no one online, but server was not used, so backup is not needed")
                 shutdown()
 
 
 def drive_backup():
     print(f"[{datetime.now()}] only backing up to Google Drive")
-    backup.main(type="drive")
+    backup.main(cfg, type="drive")
     print(f"[{datetime.now()}] shutting down...")
     shutdown()
 
@@ -72,6 +81,9 @@ def main(mode: Mode):
         print(f"[{datetime.now()}] invalid time, shutting down")
         shutdown()
         sys.exit(1)
+    elif mode == Mode.CONFIGURATION:
+        from msm.config import configuration
+        configuration.main()
 
 if __name__ == "__main__":
     mode = get_mode()
