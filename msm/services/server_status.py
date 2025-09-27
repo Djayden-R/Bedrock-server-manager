bb68ed1f43c94484fc6d74666d13f7a897ccf61d
Djayden-R <6953@rgomiddelharnis.nl>
2025-09-27 13:40:38 +0200

    check for None value's

--- msm/services/server_status.py
+++ msm/services/server_status.py
@@ -4,8 +4,11 @@ from datetime import datetime
 from msm.config.load_config import Config
 
 def start_checking_playercount(cfg: Config) -> bool:
-    server = BedrockServer(cfg.mc_ip, cfg.mc_port)
-    amount_of_checks = int(cfg.timing_shutdown / 10)
+    if cfg.mc_ip and cfg.timing_shutdown:
+        server = BedrockServer(cfg.mc_ip, cfg.mc_port)
+        amount_of_checks = int(cfg.timing_shutdown / 10)
+    else:
+        raise ValueError(f"Cannot check playercount: {'Minecraft ip not defined' if not cfg.mc_ip else ''} {'and timing not defined' if not cfg.timing_shutdown else ''}")
     
     times_no_one = 0
     server_used = False
