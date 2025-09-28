8292c6c9ad5bbea3ed9fa9743ff037257cce04bd
Djayden-R <6953@rgomiddelharnis.nl>
2025-09-28 23:34:25 +0200

    add minecraft updater dependencies to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+set -e
+
+#update and upgrade system packages
 sudo apt-get update
 sudo apt-get upgrade -y
 
-sudo apt-get install python3-venv -y
+#install required system packages for Minecraft updater
+sudo apt-get install -y python3 python3-pip python3-venv wget unzip tmux libcurl4
 
+#create and activate virtual environment
 python3 -m venv venv
 source venv/bin/activate
 
+#install and run the program
 pip install -e .
 minecraft-server-manager
