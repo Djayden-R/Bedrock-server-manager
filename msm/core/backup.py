129c93b43d126935fde80aae3891188f12132b17
Djayden-R <6953@rgomiddelharnis.nl>
2025-10-04 12:01:58 +0200

    streamline folder creation and improve symlink handling

--- msm/core/backup.py
+++ msm/core/backup.py
@@ -11,17 +11,14 @@ def generate_file_name(cfg: Config):
     date = datetime.datetime.now()
 
     folder_name = date.strftime("%Y-%m-%d")
-
     backup_name = date.strftime('backup_%H-%M-%S')
-
-    backup_path = f"{cfg.backup_local_path}/{folder_name}/{backup_name}.zip"
     
     print(f"Name of the file will be: '{backup_name}.zip'")
 
-    return backup_path, backup_name, folder_name
+    return backup_name, folder_name
 
 def generate_zip(cfg: Config, backup_name):
-    #move directories to a temp directory
+    #move directories to a temp directory and make a zip file from that temp directory
     if cfg.backup_directories:
         with tempfile.TemporaryDirectory() as temp_dir:
             for directory in cfg.backup_directories:
@@ -30,16 +27,10 @@ def generate_zip(cfg: Config, backup_name):
                 shutil.copytree(directory, temp_dest)
             shutil.make_archive(backup_name, "zip", temp_dir)
         print("Zip file generated")
+        return os.path.abspath(os.path.dirname(__file__))
     else:
         print("There are no backup directories defined")
-
-def backup_HDD(cfg: Config, backup_path, foldername):
-    #check if hdd folder exists and then make a copy of the zip file to the hdd
-    if not cfg.backup_hdd_path or not os.path.exists(cfg.backup_hdd_path):
-        raise ValueError("HDD backup directory not found")
-    else:
-        shutil.copy(backup_path, f"{cfg.backup_hdd_path}/{foldername}")
-        print("Backup copied to hardrive")
+        return False
 
 def backup_drive(cfg: Config, backup_symlink, folder, filename):
     t_beginning = monotonic()
@@ -58,7 +49,14 @@ def backup_drive(cfg: Config, backup_symlink, folder, filename):
 
 
 def update_sym_link(cfg: Config, backup_path):
-    symlink = f"{cfg.backup_local_path}/latest_backup.zip"
+    if cfg.backup_local_path:
+        location = cfg.backup_local_path
+    elif cfg.backup_hdd_path:
+        location = cfg.backup_hdd_path
+    else:
+        return
+    
+    symlink = os.path.join(location, "latest_backup.zip")
 
     #check if symlink already exists
     if os.path.islink(symlink):
@@ -72,35 +70,43 @@ def quick_backup(cfg: Config):
     #check if directories exist and if local backup doesn't, it creates it
     if not cfg.backup_directories:
         raise ValueError("Please add the directories you want to backup to 'directories'")
-    if cfg.backup_local_path:
-        if not os.path.exists(cfg.backup_local_path):
-            os.makedirs(cfg.backup_local_path)
-            print(f"Created folder: '{cfg.backup_local_path}'")
-    else:
-        print("Local backup location is not defined")
+    
+    if not (cfg.backup_local_path or cfg.backup_hdd_path):
+        return
 
     #generate name and a folder with today's date, if it doesn't exist already from an earlier backup
-    backup_path, backup_name, folder_name = generate_file_name(cfg)
+    backup_name, folder_name = generate_file_name(cfg)
 
-    local_folder = f"{cfg.backup_local_path}/{folder_name}"
-    hdd_folder = f"{cfg.backup_hdd_path}/{folder_name}"
-
-    for folder in [local_folder, hdd_folder]:
-        if not os.path.exists(folder):
-            os.makedirs(folder)
-            print(f"Created backup folder for today: '{folder}'")
+    #check if backup locations and folders exist and create them if the don't
+    for backup_location in [cfg.backup_local_path, cfg.backup_local_path]:
+        if backup_location:
+            backup_folder = os.path.join(backup_location, folder_name)
+            if not os.path.exists(backup_folder):
+                os.makedirs(backup_folder)
+                print(f"Created backup folder for today: '{backup_folder}'")
     
-    generate_zip(cfg, backup_name) #generate zip in local folder
+    temp_backup_folder = generate_zip(cfg, backup_name) #generate a temporary zip file in main folder
+    
+    if not temp_backup_folder:
+        return
 
-    backup_HDD(cfg, backup_path, folder_name) #copy the zip to hdd location
+    temp_backup_path = os.path.join(temp_backup_folder, f"{backup_name}.zip")
+    
+    #copy the temp backup to the local folder and/or the hdd folder
+    if cfg.backup_local_path:
+        backup_folder = os.path.join(cfg.backup_local_path, folder_name)
+        shutil.copy(temp_backup_path, backup_folder)
+        update_sym_link(cfg, temp_backup_path) #update symlink for later backup's
 
-    update_sym_link(cfg, backup_path) #update symlink for later backup's
+    if cfg.backup_hdd_path:
+        backup_folder = os.path.join(cfg.backup_hdd_path, folder_name)
+        shutil.copy(temp_backup_path, backup_folder)
 
-def drive_backup(cfg: Config):
+def drive_backup(cfg):
     date = datetime.datetime.now() - datetime.timedelta(days=1) #the update is from the day before, so this is calculated
     folder = date.strftime("backup/%y-%m-%d")
     filename = date.strftime("backup_%H-%M-%S.zip")
-    latest_backup_path = f"{cfg.backup_local_path}/latest_backup.zip" #refer to symlink to the latest backup
+    latest_backup_path = os.path.join(cfg.backup_local_path, "latest_backup.zip") #refer to symlink to the latest backup
 
     backup_drive(cfg, latest_backup_path, folder, filename)
 
