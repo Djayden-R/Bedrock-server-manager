5114428956adfb17b5fb9de53cfc8fd2a1ea61bc
Djayden-R <6953@rgomiddelharnis.nl>
2025-11-23 23:21:55 +0100

    add logger and test method

--- msm/services/ddns_update.py
+++ msm/services/ddns_update.py
@@ -1,20 +1,30 @@
 import requests
 from datetime import datetime
 from msm.config.load_config import Config
+import logging
+
+# Get logger
+log = logging.getLogger("bsm")
 
 url = "https://api.dynu.com/nic/update"
 
 
-def update_DNS(cfg: Config):
+def update_DNS(cfg: Config = None, test = False, domain = None, password = None):
 
     params = {
-        "hostname": cfg.dynu_domain,
-        "password": cfg.dynu_pass,
+        "hostname": cfg.dynu_domain if test else domain,
+        "password": cfg.dynu_pass if test else password,
     }
 
     response = requests.get(url, params=params)
 
     if "good" in response.text:
-        print(f"[{datetime.now()}] DDNS update successful")
+        if test:
+            return True
+        else:
+            log.info(f"[{datetime.now()}] DDNS update successful")
     else:
-        print(f"[{datetime.now()}] DDNS update failed:", response.text)
+        if test:
+            return True
+        else:
+            log.error(f"[{datetime.now()}] DDNS update failed: {response.text}")
